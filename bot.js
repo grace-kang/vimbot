f208fdf4cda5a9bc406478b5af49829b87a29b10
Grace Kang <grace@stembolt.com>
2018-07-20 15:56:06 -0700

    Add simultaneous team connections

--- bot.js
+++ bot.js
@@ -1,25 +1,6 @@
 require('dotenv').config();
 
 /**
- *  * Define a function for initiating a conversation on installation
- *   * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
- *    */
-
-function onInstallation(bot, installer) {
-  if (installer) {
-    bot.startPrivateConversation({user: installer}, function (err, convo) {
-      if (err) {
-        console.log(err);
-      } else {
-        convo.say('I am a bot that has just joined your team');
-        convo.say('You must now /invite me to a channel so that I can be of use!');
-      }
-    });
-  }
-}
-
-
-/**
  *  * Configure the persistence options
  *   */
 
@@ -31,25 +12,15 @@ if (process.env.MONGOLAB_URI) {
   };
 } else {
   config = {
-    json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
+    json_file_store: 'db'
   };
 }
 
-/**
- *  * Are being run as an app or a custom integration? The initialization will differ, depending
- *   */
-
-if (process.env.TOKEN || process.env.SLACK_TOKEN) {
-  //Treat this as a custom integration
-  var customIntegration = require('./lib/custom_integrations');
-  var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
-  var controller = customIntegration.configure(token, config, onInstallation);
-} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
-  //Treat this as an app
-  var app = require('./lib/apps');
-  var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
+  var app =  require('./lib/apps');
+  var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config);
 } else {
-  console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
+  console.log('Error: Please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
   process.exit(1);
 }
 
@@ -59,14 +30,6 @@ require("fs").readdirSync(normalizedPath).forEach(function(file) {
 });
 
 
-/**
- * A demonstration for how to handle websocket events. In this case, just log when we have and have not
- * been disconnected from the websocket. In the future, it would be super awesome to be able to specify
- * a reconnect policy, and do reconnections automatically. In the meantime, we aren't going to attempt reconnects,
- * WHICH IS A B0RKED WAY TO HANDLE BEING DISCONNECTED. So we need to fix this.
- *
- * TODO: fixed b0rked reconnect behavior
- */
 // Handle events related to the websocket connection to Slack
 controller.on('rtm_open', function (bot) {
   console.log('** The RTM api just connected!');
@@ -76,3 +39,4 @@ controller.on('rtm_close', function (bot) {
   console.log('** The RTM api just closed');
   // you may want to attempt to re-open
 });
+
